40598740692f07292e4b7c99d235a0e920ea9240
farismnrr <farismunir2@gmail.com>
2024-09-22 21:08:21 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data } = await useAsyncData("home", () => queryContent("/").findOne());
+</script>
+<template>
+    <div>
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.description }}</p>
+    </div>
+</template>
